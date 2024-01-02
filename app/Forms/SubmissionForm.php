ee9f45356429962cbb947691eff51bc2aecc22c6
kkfender <ibanez19851030@yahoo.co.jp>
2024-01-02 16:02:03 +0900

    編集画面の改修

--- app/Forms/SubmissionForm.php
+++ app/Forms/SubmissionForm.php
@@ -4,6 +4,7 @@ namespace App\Forms;
 
 use Kris\LaravelFormBuilder\Form;
 use App\Models\Prefecture;
+use App\Models\Board;
 
 class SubmissionForm extends Form
 {
@@ -12,31 +13,39 @@ class SubmissionForm extends Form
         $prefectures = Prefecture::get();
         $prefectures = $prefectures->pluck('name','id')->toArray();
 
+        $board = Board::find($this->data['id']);
+
         $this
             ->add('title', 'text', [
                 'label' => '題名',
-                'rules' => 'required' 
+                'rules' => 'required',
+                'default_value' => $board->title,
             ])
             ->add('name', 'text', [
                 'label' => '名前',
-                'rules' => 'required'
+                'rules' => 'required',
+                'default_value' => $board->name,
             ])
             ->add('email', 'email', [
                 'label' => 'メールアドレス',
-                'rules' => 'required|email'
+                'rules' => 'required|email',
+                'default_value' => $board->email,
             ])
             ->add('prefecture_id', 'select', [
                 'label' => '場所',
                 'rules' => 'required',
                 'choices' => $prefectures,
+                'default_value' => $board->prefecture_id,
             ])
             ->add('body', 'textarea', [
                 'label' => '本文',
                 'rules' => 'required',
+                'default_value' => $board->body,
             ])
             ->add('operation_key', 'text', [
                 'label' => '編集削除キー',
                 'rules' => 'required|integer',
+                'default_value' => $board->operation_key,
             ])
             ->add('submit', 'submit', ['label' => '保存']);
     }
